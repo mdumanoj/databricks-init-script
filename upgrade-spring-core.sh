4b89375128c726798f0e06ba0d428a816092e5e4
Manoj Kumar <sachinmanojj@gmail.com>
2022-03-26 15:19:37 +0530

    Update upgrade-spring-core.sh

--- upgrade-spring-core.sh
+++ upgrade-spring-core.sh
@@ -1,3 +1,3 @@
-%sh
+#!/bin/bash
 rm -rf ----workspace_spark_3_2--maven-trees--hive-2.3__hadoop-3.2--org.springframework--spring-core--org.springframework__spring-core__4.1.4.RELEASE.jar
 cp /dbfs/FileStore/jars/54727eaf_e229_4a68_a0ae_eb768901055e-spring_core_4_3_5_RELEASE-9c98f.jar /databricks/jars/
